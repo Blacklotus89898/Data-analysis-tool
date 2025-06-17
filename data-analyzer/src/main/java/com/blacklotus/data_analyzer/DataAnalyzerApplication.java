357c6f4e149f7f587afefcdaf75740d10bdeaf2c
Blacklotus89898 <song.yang.chen89898@gmail.com>
2025-06-17 14:34:07 -0400

    Feat: project initialization

---
+++ data-analyzer/src/main/java/com/blacklotus/data_analyzer/DataAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.blacklotus.data_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalyzerApplication.class, args);
+	}
+
+}
---
+++ data-analyzer/src/test/java/com/blacklotus/data_analyzer/DataAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacklotus.data_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
